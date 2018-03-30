96bd0298fe6155371eed304246ad71ac25bef51b
CEbbinghaus <cebbinghaus@live.de>
2018-03-30 15:15:48 +1100

    Fixed Bugs given to me by Tombez

--- lib.js
+++ lib.js
@@ -4,14 +4,9 @@ const generateId = (l = 8) => {
     return l.loop(() => randChar(100));
 }
 const randChar = (n = 26, sn) => {
-    let a = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJLLMNOPQRSTUVWXYZ";
+    let a = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJkLMNOPQRSTUVWXYZ";
     let t = sn ? a.slice(n, sn) : a.slice(0, n);
     return t.split("").splice(Math.random() * t.length | 0)[0];
-}
-String.prototype.replaceFrom = function(i, ss, rs){
-    let parts = [this.slice(0, i), this.slice(i)];
-    parts[1].replace(ss, rs);
-    return parts.join("");
 }
 String.prototype.insert = function(i, t){
     let parts = [this.slice(0, i), this.slice(i)];
@@ -33,7 +28,6 @@ Object.prototype.keys = function(){
 Object.prototype.keyLength = function(){
     return Object.keys(this).length;
 }
-Object.prototype.test = this.keys();
 Number.prototype.loop = function(c){
     if(!c)throw "Needs a Callback";
     let r;
